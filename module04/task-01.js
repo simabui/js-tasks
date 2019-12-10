80423fd98717bfa1085780e7269e31a6d9a6b76a
simabui <sima_sima@live.ru>
2019-12-10 23:50:40 +0200

    edit task01

--- module04/task-01.js
+++ module04/task-01.js
@@ -1,19 +1,19 @@
 "use sctrict";
 const account = {
-	owner: "Mango",
-	balance: 24000,
-	discount: 0.1,
-	orders: ["order-1", "order-2", "order-3"],
-	changeDiscount(value) {
-		this.discount = value;
-	},
-	getOrders() {
-		return this.orders;
-	},
-	addOrder(cost, order) {
-		this.balance -= cost;
-		orders.push(order);
-	}
+  owner: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["order-1", "order-2", "order-3"],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    orders.push(order);
+  }
 };
 
 account.changeDiscount(0.15);
