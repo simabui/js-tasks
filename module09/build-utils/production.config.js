c78782cd70d53eebd98013ab8af4aea10dc696dc
simabui <sima_sima@live.ru>
2020-01-09 00:30:28 +0200

    add webpack buildpack

---
+++ module09/build-utils/production.config.js
@@ -0,0 +1,36 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "css/styles.css" }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default", { discardComments: { removeAll: true } }]
+      },
+      canPrint: true
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pug/index.pug"
+    })
+  ],
+  devServer: {
+    stats: "errors-only"
+  }
+};
