77acef5a0d6126c615d95f9a4987ab5220d3c479
simabui <sima_sima@live.ru>
2020-01-09 21:45:38 +0200

    add new webpack

---
+++ module09/src/js/func.js
@@ -0,0 +1,5 @@
+const toArrow = (a, b) => {
+  return a + b;
+};
+
+export default toArrow;
---
+++ module09/src/js/index.js
@@ -0,0 +1,8 @@
+import message from "./script";
+import func from "./func";
+import "./../sass/styles.scss";
+import "./../images/team-1.jpg";
+
+console.log(func(1, 2));
+console.log(message);
+console.log("testtest");
