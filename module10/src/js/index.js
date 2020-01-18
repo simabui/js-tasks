3967872df500ca43834e44039cdb9f0c258a6c45
simabui <sima_sima@live.ru>
2020-01-18 18:10:09 +0200

    removed files

--- module10/src/js/index.js
+++ module10/src/js/index.js
@@ -1,8 +1,1 @@
-import message from "./script";
-import func from "./func";
 import "./../sass/styles.scss";
-import "../images/team-1.jpg";
-
-console.log(func(1, 2));
-console.log(message);
-console.log("testtest");
