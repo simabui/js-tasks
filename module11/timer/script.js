e8feed550112cea03cd574670289b00a456eb24c
simabui <sima_sima@live.ru>
2020-01-19 21:25:31 +0200

    fix interval

--- module11/timer/script.js
+++ module11/timer/script.js
@@ -16,14 +16,16 @@ class CountdownTimer {
   }
 
   countTime() {
-    this.currentDate = Date.now();
-    let time = this.targetDate - this.currentDate;
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-
     setInterval(() => {
+      this.currentDate = Date.now();
+      let time = this.targetDate - this.currentDate;
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+      const secs = Math.floor((time % (1000 * 60)) / 1000);
+
       refs.days.textContent = String(days);
       refs.hours.textContent = String(hours);
       refs.mins.textContent = String(mins);
