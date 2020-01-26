59740aa2c8aa14f02e789b14bd5d1b8ba0ea677e
simabui <sima_sima@live.ru>
2020-01-26 20:11:41 +0200

    add debounce

--- module12/src/js/handle-search.js
+++ module12/src/js/handle-search.js
@@ -1,24 +1,22 @@
 import fetching from "./countries-service.js";
 import countriesTemplate from "../templates/countries-temp.hbs";
+import debounce from "../../node_modules/lodash.debounce";
 
 const refs = {
   input: document.querySelector("#country-search"),
   list: document.querySelector(".countries__list")
 };
 
-refs.input.addEventListener("input", handlerInput);
+refs.input.addEventListener("input", debounce(handlerInput, 500));
 
 function handlerInput(e) {
   const input = e.target.value;
 
   fetching.fetchCountries(input).then(country => {
-    const markUp = renderTemplate(country);
+    const markUp = countriesTemplate(country);
+    markUp;
     buildMarkUp(markUp);
   });
-}
-
-function renderTemplate(data) {
-  return countriesTemplate(data);
 }
 
 function buildMarkUp(data) {
