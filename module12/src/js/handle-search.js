af7e8ddcefa2b0a60e9249e0216e5c1fe9e91f49
simabui <sima_sima@live.ru>
2020-01-26 20:01:30 +0200

    render template

--- module12/src/js/handle-search.js
+++ module12/src/js/handle-search.js
@@ -1,8 +1,9 @@
 import fetching from "./countries-service.js";
-import countriesTemplate from "../templates/country-list.hbs";
+import countriesTemplate from "../templates/countries-temp.hbs";
 
 const refs = {
-  input: document.querySelector("#country-search")
+  input: document.querySelector("#country-search"),
+  list: document.querySelector(".countries__list")
 };
 
 refs.input.addEventListener("input", handlerInput);
@@ -10,12 +11,17 @@ refs.input.addEventListener("input", handlerInput);
 function handlerInput(e) {
   const input = e.target.value;
 
-  const markUp = fetching
-    .fetchCountries(input)
-    .then(country => renderTemplate(country));
-  console.log(markUp);
+  fetching.fetchCountries(input).then(country => {
+    const markUp = renderTemplate(country);
+    buildMarkUp(markUp);
+  });
 }
 
 function renderTemplate(data) {
   return countriesTemplate(data);
+}
+
+function buildMarkUp(data) {
+  refs.list.innerHTML = "";
+  refs.list.insertAdjacentHTML("beforeend", data);
 }
