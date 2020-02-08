5ad155a90d28381bb24f586314e71f8bfecf07f4
simabui <sima_sima@live.ru>
2020-02-08 12:02:57 +0200

    update styles and add loader wheel

--- module13/src/js/apiService.js
+++ module13/src/js/apiService.js
@@ -5,11 +5,13 @@ import InfiniteScroll from "infinite-scroll";
 import collectionTemplate from "../templates/collections-temp.hbs";
 
 const baseURL =
-  "https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12&order=popular";
+  "https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12&order=popular";
+const CORSPROXY = "https://cors-anywhere.herokuapp.com/";
 const KEY = "&key=15109703-4df3afa39634f93d9eb19fc69";
 let input = "";
 const grid = document.querySelector(".gallery");
 const button = document.querySelector("#load");
+const loadError = document.querySelector(".infinite-scroll-last");
 
 export function recieveValue(value) {
   return (input = value);
@@ -28,8 +30,11 @@ const msnryOptions = {
 const scrollOptions = {
   responseType: "text",
   history: false,
+  status: ".loader-wheel",
   path() {
-    return `${baseURL + KEY}` + `&q=${input}` + `&page=${this.pageIndex}`;
+    return (
+      `${CORSPROXY + baseURL + KEY}` + `&q=${input}` + `&page=${this.pageIndex}`
+    );
   }
 };
 
@@ -40,11 +45,14 @@ export function fetchResponse() {
 
   infScroll.on("load", response => {
     const images = JSON.parse(response);
+    const markup = images.hits.map(image => collectionTemplate(image)).join("");
+
     // Pnotify
     if (images.hits.length > 1) success();
-    if (images.hits.length < 1) alert();
-
-    const markup = images.hits.map(image => collectionTemplate(image)).join("");
+    if (images.hits.length < 1) {
+      alert();
+      loadError.style.display = "block";
+    }
 
     const placeholder = document.createElement("div");
     placeholder.innerHTML = markup;
