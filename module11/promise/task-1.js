3074822aa9c476fbe649de8696ca3af66941ab59
simabui <sima_sima@live.ru>
2020-02-11 21:59:48 +0200

    update promise

--- module11/promise/task-1.js
+++ module11/promise/task-1.js
@@ -1,16 +1,23 @@
 "use strict";
 
 const delay = ms => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(ms);
+      reject("Error in function");
     }, ms);
   });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
-
+const rejectLog = err => console.log(`Error: ${err}`);
 // Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000)
+  .then(logger)
+  .catch(rejectLog); // Resolved after 2000ms
+delay(1000)
+  .then(logger)
+  .catch(rejectLog); // Resolved after 1000ms
+delay(1500)
+  .then(logger)
+  .catch(rejectLog); // Resolved after 1500ms
